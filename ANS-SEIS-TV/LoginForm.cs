65b9a177d6da5fc5d46f4f1a971bf03c1014a6fe
Nathaniel Angelico Tabanao <nathanieltabanao@gmail.com>
2018-07-03 17:44:54 +0800

    Login Form

---
+++ ANS-SEIS-TV/LoginForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANS_SEIS_TV
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();            
+        }
+
+        //loginbutton
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
