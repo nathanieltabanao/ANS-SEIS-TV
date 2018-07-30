29e215e294a85c644c6aecd21eb6f90cbc1fb30b
Nathaniel Angelico Tabanao <nathanieltabanao@gmail.com>
2018-07-30 13:30:26 +0800

    Fixed bugs on current user

--- ANS-SEIS-TV/GetSomethingFromServer.cs
+++ ANS-SEIS-TV/GetSomethingFromServer.cs
@@ -11,12 +11,18 @@ namespace ANS_SEIS_TV
     {
         DataClasses1DataContext db = new DataClasses1DataContext();
 
+        
+
         public string Username { get; set; }
         public string ID { get; set; }
         public int FirstLogin { get; set; }
         public int UsertypeID { get; set; }
         public int GENID { get; set; }
         public string Fullname { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
 
         public void GetSomething()
         {
@@ -57,11 +63,43 @@ namespace ANS_SEIS_TV
 
         public void GetFullname()
         {
+            //var results = from TBLUSERDETAIL in db.TBLUSERDETAILs
+            //              where TBLUSERDETAIL.USERNAME == Username
+            //              select new { Fullname = TBLUSERDETAIL.FIRSTNAME + " " + TBLUSERDETAIL.MIDDLENAME + " " + TBLUSERDETAIL.LASTNAME };
+
+            //Fullname = results.FirstOrDefault().ToString();
+
+            GetFirstName();
+            GetMiddleName();
+            GetLastName();
+            Fullname = FirstName + " " + MiddleName + " " + LastName;
+        }
+
+        public void GetFirstName()
+        {
             var results = from TBLUSERDETAIL in db.TBLUSERDETAILs
                           where TBLUSERDETAIL.USERNAME == Username
-                          select new { Fullname = TBLUSERDETAIL.FIRSTNAME + " " + TBLUSERDETAIL.MIDDLENAME + " " + TBLUSERDETAIL.LASTNAME };
+                          select TBLUSERDETAIL.FIRSTNAME;
+                
+            FirstName = results.FirstOrDefault().ToString();
+        }
 
-            Fullname = results.FirstOrDefault().ToString();
+        public void GetMiddleName()
+        {
+            var results = from TBLUSERDETAIL in db.TBLUSERDETAILs
+                          where TBLUSERDETAIL.USERNAME == Username
+                          select TBLUSERDETAIL.MIDDLENAME;
+
+            MiddleName = results.FirstOrDefault().ToString();
+        }
+
+        public void GetLastName()
+        {
+            var results = from TBLUSERDETAIL in db.TBLUSERDETAILs
+                          where TBLUSERDETAIL.USERNAME == Username
+                          select TBLUSERDETAIL.LASTNAME;
+
+            LastName = results.FirstOrDefault().ToString();
         }
     }
 }
