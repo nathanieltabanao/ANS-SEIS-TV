3a7783653581d56163bde4d0ae466398a96e2b48
Nathaniel Angelico Tabanao <nathanieltabanao@gmail.com>
2018-10-02 17:36:38 +0800

    Ready for Submission, Maybe?

---
+++ ANS-SEIS-TV/frmDeploymentReport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ANS_SEIS_TV
+{
+    public partial class frmDeploymentReport : MetroForm
+    {
+        int AdminID;
+        string AdminName;
+        DateTime Date;
+
+        GetSomethingFromServer g = new GetSomethingFromServer();
+
+        public frmDeploymentReport(int Admin, DateTime Now)
+        {
+            InitializeComponent();
+
+            AdminID = Admin;
+            Date = Now;
+            g.GetUsername(AdminID);
+            g.GetFullname();
+            AdminName = g.Fullname;
+        }
+
+        private void frmDeploymentReport_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            rptDeploymentReport1.SetParameterValue("pProcessedBy", AdminName);
+            rptDeploymentReport1.SetParameterValue("pDateBorrowed", Date);
+            crystalReportViewer1.ReportSource = rptDeploymentReport1;
+        }
+    }
+}
---
+++ ANS-SEIS-TV/frmReservationReport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace ANS_SEIS_TV
+{
+    public partial class frmReservationReport : MetroForm
+    {
+        int AdminID;
+        string AdminName;
+        DateTime Date;
+
+        GetSomethingFromServer g = new GetSomethingFromServer();
+
+        public frmReservationReport(int Admin, DateTime Now)
+        {
+            InitializeComponent();
+
+            AdminID = Admin;
+            Date = Now;
+            g.GetUsername(AdminID);
+            g.GetFullname();
+            AdminName = g.Fullname;
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            rptEquipmentReservation1.SetParameterValue("pProcessedBy", AdminName);
+            rptEquipmentReservation1.SetParameterValue("pDateBorrowed", Date);
+            crystalReportViewer1.ReportSource = rptEquipmentReservation1;
+        }
+    }
+}
