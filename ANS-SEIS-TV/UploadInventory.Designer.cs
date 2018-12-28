fc9318ca5bd73a45dc7f37f6ca3805118c54dc9f
Nathaniel Angelico Tabanao <nathanieltabanao@gmail.com>
2018-12-28 17:59:58 +0800

    Upload User and Equipment

---
+++ ANS-SEIS-TV/UploadInventory.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ANS_SEIS_TV
+{
+    partial class UploadInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvInventoryUpload = new MetroFramework.Controls.MetroGrid();
+            this.txtFileLocation = new System.Windows.Forms.TextBox();
+            this.btnOpenFileDialog = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btnPreview = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSheetName = new System.Windows.Forms.TextBox();
+            this.LocationToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.btnUpload = new MaterialSkin.Controls.MaterialFlatButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventoryUpload)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInventoryUpload
+            // 
+            this.dgvInventoryUpload.AllowUserToResizeRows = false;
+            this.dgvInventoryUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvInventoryUpload.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvInventoryUpload.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvInventoryUpload.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvInventoryUpload.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvInventoryUpload.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvInventoryUpload.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvInventoryUpload.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvInventoryUpload.EnableHeadersVisualStyles = false;
+            this.dgvInventoryUpload.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgvInventoryUpload.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgvInventoryUpload.Location = new System.Drawing.Point(23, 154);
+            this.dgvInventoryUpload.Name = "dgvInventoryUpload";
+            this.dgvInventoryUpload.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvInventoryUpload.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvInventoryUpload.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvInventoryUpload.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvInventoryUpload.Size = new System.Drawing.Size(702, 317);
+            this.dgvInventoryUpload.TabIndex = 0;
+            // 
+            // txtFileLocation
+            // 
+            this.txtFileLocation.Location = new System.Drawing.Point(53, 89);
+            this.txtFileLocation.Name = "txtFileLocation";
+            this.txtFileLocation.Size = new System.Drawing.Size(671, 20);
+            this.txtFileLocation.TabIndex = 1;
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(24, 86);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(24, 25);
+            this.btnOpenFileDialog.TabIndex = 2;
+            this.btnOpenFileDialog.Values.Text = "*";
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(634, 125);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(90, 25);
+            this.btnPreview.TabIndex = 3;
+            this.btnPreview.Values.Text = "Preview";
+            this.btnPreview.Click += new System.EventHandler(this.kryptonButton2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel Worksheets|*.xls; *xlsx";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sheet Name";
+            // 
+            // txtSheetName
+            // 
+            this.txtSheetName.Location = new System.Drawing.Point(53, 128);
+            this.txtSheetName.Name = "txtSheetName";
+            this.txtSheetName.Size = new System.Drawing.Size(575, 20);
+            this.txtSheetName.TabIndex = 5;
+            // 
+            // LocationToolTip
+            // 
+            this.LocationToolTip.ToolTipTitle = "Find File";
+            this.LocationToolTip.Popup += new System.Windows.Forms.PopupEventHandler(this.LocationToolTip_Popup);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpload.AutoSize = true;
+            this.btnUpload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnUpload.Depth = 0;
+            this.btnUpload.Icon = null;
+            this.btnUpload.Location = new System.Drawing.Point(611, 480);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnUpload.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Primary = false;
+            this.btnUpload.Size = new System.Drawing.Size(113, 36);
+            this.btnUpload.TabIndex = 7;
+            this.btnUpload.Text = "Upload Data";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // UploadInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 528);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSheetName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.txtFileLocation);
+            this.Controls.Add(this.dgvInventoryUpload);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "UploadInventory";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.ShowInTaskbar = false;
+            this.Text = "Invetory Upload";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventoryUpload)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid dgvInventoryUpload;
+        private System.Windows.Forms.TextBox txtFileLocation;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnOpenFileDialog;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnPreview;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSheetName;
+        private System.Windows.Forms.ToolTip LocationToolTip;
+        private MaterialSkin.Controls.MaterialFlatButton btnUpload;
+    }
+}
---
+++ ANS-SEIS-TV/UploadUsers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using MetroFramework;
+using MetroFramework.Forms;
+using System.Globalization;
+using System.IO;
+using System.Data.OleDb;
+using System.Runtime.InteropServices;
+
+namespace ANS_SEIS_TV
+{
+    public partial class UploadUsers : MetroForm
+    {
+        public UploadUsers()
+        {
+            InitializeComponent();
+
+            this.BorderStyle = MetroFormBorderStyle.FixedSingle;
+            this.ShadowType = MetroFormShadowType.AeroShadow;
+        }
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFileLocation.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            // name is sheetname
+            String name = txtSheetName.Text.Trim();
+
+            
+            // path
+            String locationPath = txtFileLocation.Text;
+
+            if (string.IsNullOrWhiteSpace(locationPath))
+            {
+                MetroMessageBox.Show(this, "Location is Empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(name))
+            {
+                MetroMessageBox.Show(this, "Sheet name can't be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // C:// could be  path
+                // C:\\Users\Nathaniel Angelico\Desktop\\
+                String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + locationPath + ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+
+                OleDbConnection con = new OleDbConnection(constr);
+                OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+
+                con.Open();
+
+                OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+                DataTable data = new DataTable();
+                try
+                {
+                    sda.Fill(data);
+                }
+                catch (COMException ce)
+                {
+                    if (ce.ErrorCode ==Convert.ToInt32(0x80004005))
+                    {
+                        MetroMessageBox.Show(this, "Incorrect Sheet Name or Sheet Name not Found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                finally
+                {
+                    dgvInventoryUpload.DataSource = data;
+                }
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
